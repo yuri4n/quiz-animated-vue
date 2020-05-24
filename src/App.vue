083a3745a27aadf614064a82a14ac966dbb014ee
Julian Garzon <bryangarzon22@gmail.com>
2020-05-23 21:17:35 -0500

    Initial commit
    Works as Tailwind template

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mx-auto my-5">
+    <input
+      class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+      type="email"
+      placeholder="jane@example.com"
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
